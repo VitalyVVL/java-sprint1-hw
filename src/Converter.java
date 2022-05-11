aecfb2173ef920d64a6d27bdb983c4c222a2af6f
Vvl <V-v-l@mail.ru>
2022-05-11 09:57:38 +0300

    java-sprint1-hw

---
+++ src/Converter.java
@@ -0,0 +1,18 @@
+public class Converter {
+    int lenStep;
+    int countCal;
+
+    public Converter(int lenStep, int countCol) {
+        this.lenStep = lenStep;
+        this.countCal = countCol;
+    }
+
+    public double getLenStep(int steps) {
+        return this.lenStep * steps / 100000;
+    }
+
+    public long getСalories(int steps) {
+        return this.countCal * steps/1000;
+    }
+
+}
